a4dde9b9d14b2c880021442964b6ba922a162d97
colin <colin@thinkr.fr>
2023-10-17 20:50:47 +0200

    preload funs example

---
+++ webr-preload-funs/index.js
@@ -0,0 +1,61 @@
+const app = require('express')()
+const path = require('path');
+const { loadPackages, loadFolder } = require('webrtools');
+const { WebR } = require('webr');
+
+(async () => {
+  globalThis.webR = new WebR();
+  await globalThis.webR.init();
+
+  console.log("🚀 webR is ready 🚀");
+
+  await loadPackages(
+    globalThis.webR,
+    path.join(__dirname, 'webr_packages')
+  )
+
+  await loadFolder(
+    globalThis.webR,
+    path.join(__dirname, 'rfuns'),
+    "/home/web_user"
+  )
+
+  console.log("📦 Packages written to webR 📦");
+
+  // see https://github.com/r-wasm/webr/issues/292
+  await globalThis.webR.evalR("options(expressions=1000)")
+  await globalThis.webR.evalR("pkgload::load_all('/home/web_user')");
+
+  app.listen(3000, '0.0.0.0', () => {
+    console.log('http://localhost:3000')
+  })
+
+})();
+
+app.get('/', async (req, res) => {
+  let result = await globalThis.webR.evalR(
+    'unique(dplyr::starwars$species)'
+  );
+  try {
+    let js_res = await result.toJs()
+    res.send(js_res.values)
+  } finally {
+    webR.destroy(result);
+  }
+
+})
+
+
+app.get('/:n', async (req, res) => {
+  let result = await globalThis.webR.evalR(
+    'star_wars_by_species(n)',
+    { env: { n: req.params.n } }
+  );
+  console.log(result)
+  try {
+    const result_js = await result.toJs();
+    res.send(result_js)
+  } finally {
+    webR.destroy(result);
+  }
+});
---
+++ webr-preload-funs/rfuns/R/sw.R
@@ -0,0 +1,15 @@
+#' @title Star Wars by Species
+#' @description Return a tibble of Star Wars characters by species
+#' @import dplyr
+#' @export
+#' @param species character
+#' @return tibble
+#' @examples
+#' star_wars_by_species("Human")
+#' star_wars_by_species("Droid")
+#' star_wars_by_species("Wookiee")
+#' star_wars_by_species("Rodian")
+star_wars_by_species <- function(species) {
+  dplyr::starwars |>
+    filter(species == {{ species }})
+}
